92b77c8c61ba36d4374651af852d975439bf7b14
edwardbudaza <edward.budaza@yahoo.co.za>
2024-04-09 02:27:08 +0200

    secretes concealed

--- vm/settings.py
+++ vm/settings.py
@@ -13,12 +13,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "j9h=s!uu*pf%8p#mkd_tdw#ek*+ite7#f5gxg7i%sw64_c^0kx"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
